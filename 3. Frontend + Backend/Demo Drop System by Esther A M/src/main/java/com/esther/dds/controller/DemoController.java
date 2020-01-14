70cdd7df6b7b37fa10e3e935d04f67dbe85304fd
Esther <esther191299@gmail.com>
2020-01-14 08:29:35 +0100

    State radio checkbox + css + Bo mappings

--- 3. Frontend + Backend/Demo Drop System by Esther A M/src/main/java/com/esther/dds/controller/DemoController.java
+++ 3. Frontend + Backend/Demo Drop System by Esther A M/src/main/java/com/esther/dds/controller/DemoController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
@@ -31,16 +32,18 @@ public class DemoController {
     }
 
 
-    // List of Demos on dashboard
+//     DASHBOARD.HTML
+    // List of Demos
     @GetMapping("/dashboard")
-    public String userList(Model model){
+    public String userSideList(Model model){
         model.addAttribute("demos", demoRepository.findAll());
         return "dashboard";
     }
 
-    // Demo view
+//     VIEWDEMO.HTML
+    // Play
     @GetMapping("/demo/{id}")
-    public String viewDemo (@PathVariable Long id, Model model){
+    public String userSideDemo (@PathVariable Long id, Model model){
         Optional<Demo> demo = demoRepository.findById(id);
         if( demo.isPresent() ) {
             model.addAttribute("demo",demo.get());
@@ -52,44 +55,115 @@ public class DemoController {
         }
     }
 
-    // Demo drop form
+//     DROPDEMO.HTML
+    // Load new Demo-object in form
     @GetMapping("/dropdemo")
     public String newDemoForm(Model model){
         model.addAttribute("demo", new Demo());
         return "dropdemo";
     }
 
+
+//    @PostMapping("/dropdemo")
+//    public String uploadFile(@RequestParam("audioFile") MultipartFile audioFile){
+//        String value = "";
+//        audioUploadService.saveAudio(audioFile);
+//        // save multipart file to folder
+//
+//
+//        // get path (string) of multipartfile
+//        return value;
+//    }
+
+
+//     DROPDEMO.HTML -> Post
+    // Bind form loaded in to object
     @PostMapping("/dropdemo")
-    public String createDemo(@Valid Demo demo, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+    public String uploadDemo(@Valid Demo demo, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
         if( bindingResult.hasErrors() ) {
             logger.info("Validation errors were found while submitting a new demo.");
             model.addAttribute("demo",demo); //keep data when error occurs & page refreshes
             return "dropdemo";
         } else {
-            // save uploaded demo
-            demoRepository.save(demo);
-            // assign this demo to pending state
-            demo.setState(databaseFiller.state1);
-            // save demo again (update)
+
+            // save uploaded demo (title, description.)
             demoRepository.save(demo);
 
+            // assign this demo to pending state
+            demo.setState(databaseFiller.state1);
+
+            // save demo again (update: + state)
+            demoRepository.save(demo);
+
+            // get multipartFile Path in string
+
+            // assign the multipartString to demo
+            demo.setAudioFile("/serverside_audiofiles/" + "variable to path" + "variable .getdemoId");
+
+            // save demo again (update: + fileLocation. Save complete)
+            demoRepository.save(demo);
+
+
+            //log event
             logger.info("New Demo was saved successfully");
             redirectAttributes
                     .addAttribute("id",demo.getId())
                     .addFlashAttribute("success",true);
             return "redirect:/demo/{id}";
         }
+
+    }
+
+    //     BO - DASHBOARD.HTML
+    // List of Demos
+    @GetMapping("/bo/review-list")
+    public String boSideList(Model model){
+        model.addAttribute("demos", demoRepository.findAll());
+        return "bo/review-list";
+    }
+
+    //     VIEWDEMO.HTML
+    // Play
+    @GetMapping("/bo/{id}")
+    public String boSideDemo (@PathVariable Long id, Model model){
+        Optional<Demo> demo = demoRepository.findById(id);
+        if( demo.isPresent() ) {
+            model.addAttribute("demo",demo.get());
+            return "bo/review-mode";
+        }else {
+        return "redirect:/";
+        }
+    }
+
+
+    @PutMapping("/bo/{id}")
+    public String updateState(@Valid Demo demo, Demo nextDemo, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        if( bindingResult.hasErrors() ) {
+            logger.info("Validation errors were found while setting a state to the existing demo.");
+            model.addAttribute("demo",demo); //keep data when error occurs & page refreshes
+            return "bo/review-mode";
+        } else {
+
+            // assign this demo to rejected state
+            demo.setState(databaseFiller.state2);
+
+            // update state
+            demoRepository.save(demo);
+
+
+            //log event
+            logger.info("Demo has been succesfully assigned new state: Rejected");
+
+            redirectAttributes
+                    .addAttribute("id",nextDemo.getId());
+            return "redirect:/bo/{id}";
+        }
+
     }
 
 
 
-
-
-
-
-
-//
-//    // CRUD
+//    //REFERENCE
 //
 //    @PostMapping("/create")
 //    public Demo create(@ModelAttribute Demo demo) {
@@ -111,15 +185,7 @@ public class DemoController {
 //    public void delete(@PathVariable Long id) {
 //        demoRepository.deleteById(id);
 //    }
-//
-//
-//
-//
-//    //u_referenceMapping
-//    @GetMapping("/thymeleaf/u_reference")
-//    public String reference(Model model){
-//        model.addAttribute("pageTitle", "reference");
-//        return "/thymeleaf/u_reference";
-//    }
+
+
 
 }
---
+++ 3. Frontend + Backend/Demo Drop System by Esther A M/src/main/java/com/esther/dds/domain/AudioFile.java
@@ -0,0 +1,9 @@
+package com.esther.dds.domain;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class AudioFile {
+
+    private MultipartFile audioFile;
+
+}
