28903c226afa65543a2d83059f62d171507b567c
Esther <esther191299@gmail.com>
2020-01-16 16:56:58 +0100

    Security: Actuator set to only Admin

--- 3. Frontend + Backend/Demo Drop System by Esther A M/src/main/java/com/esther/dds/security/SecurityConfiguration.java
+++ 3. Frontend + Backend/Demo Drop System by Esther A M/src/main/java/com/esther/dds/security/SecurityConfiguration.java
@@ -1,6 +1,6 @@
 package com.esther.dds.security;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,8 +18,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //set all mapplings & their permissions
         http
                 .authorizeRequests()
+                .requestMatchers(EndpointRequest.to("info")).permitAll()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
+                .antMatchers("/actuator/").hasRole("ADMIN")
                 .antMatchers("/").permitAll()
                 .antMatchers("/dashboard").hasRole("USER")
                 .and()
