f213adefbad45af36a00fcd179efb8bd41ba616d
Esther <esther191299@gmail.com>
2020-01-28 03:22:28 +0100

    Added Admin-side: Set-system texts. + Refactored code

--- 3. Frontend + Backend/Demo Drop System by Esther A M/src/main/java/com/esther/dds/controller/StateController.java
+++ 3. Frontend + Backend/Demo Drop System by Esther A M/src/main/java/com/esther/dds/controller/StateController.java
@@ -1,59 +1,48 @@
 package com.esther.dds.controller;
 
-import com.esther.dds.automated.DatabaseFiller;
 import com.esther.dds.domain.State;
 import com.esther.dds.service.StateService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class StateController {
     private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
-
-
     private StateService stateService;
-    private DatabaseFiller databaseFiller;
 
-    public StateController(StateService stateService, DatabaseFiller databaseFiller) {
+    public StateController(StateService stateService) {
         this.stateService = stateService;
-        this.databaseFiller = databaseFiller;
     }
 
 
     //     SET-TEXTS.HTML
-    //todo: get texts + set texts
     @GetMapping("/admin-side/authorized/set-texts")
     public String loadTextForms(Model model){
 
         model.addAttribute("pending", stateService.findByStateName("Pending"));
-        model.addAttribute("success", stateService.findByStateName("Success"));
-        model.addAttribute("reject", stateService.findByStateName("Reject"));
+        model.addAttribute("sent", stateService.findByStateName("Sent"));
+        model.addAttribute("rejected", stateService.findByStateName("Rejected"));
         return "bo/a_set-texts";
     }
 
 
-
     //     SET-TEXTS.HTML
-    //post stuff
     @PostMapping("/admin-side/authorized/set-texts/{state}")
-    public String updatePendingState(Model model, @PathVariable("state") String stateName){
+    public String updatePendingState(Model model, @PathVariable("state") String stateName, @RequestParam(value = "message")String message){
 
         State state = stateService.findByStateName(stateName);
 
-        state.setId(state.getId());
-        state.setStateName(state.getStateName());
-
+        state.setMessage(message);
         stateService.save(state);
-
-        //the only way to update this is to somehow get the value in th form
-        //And use .setMessage on this particular State
+        logger.info("state message: '" + stateName + "' was updated successfully");
 
         return "redirect:/admin-side/authorized/set-texts";
     }
-
 
 }
